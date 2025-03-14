c630e5ba7ddf3977c03ddd053a6b42d8567fc07f
mayk mendoza <miky14n@gmail.com>
2025-03-13 22:46:24 -0400

    Fixed conection string

--- src/app/lib/neon.js
+++ src/app/lib/neon.js
@@ -1,2 +1,4 @@
 import { neon } from "@neondatabase/serverless";
-export const neon_sql = neon(process.env.DATABASE_URL);
+export const neon_sql = neon(
+  "postgresql://riego-db_owner:npg_2w8xyJzqrXuR@ep-rapid-moon-acr6g6lx-pooler.sa-east-1.aws.neon.tech/riego-db?sslmode=require"
+);
